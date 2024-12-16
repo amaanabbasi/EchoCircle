88270c72a6c5ab8e1fbbe163c1e7a6176b0705e3
dev640web <y.lu.449@westcliff.edu>
2024-12-15 20:30:05 -0800

    fix signup error

--- signup.php
+++ signup.php
@@ -1,52 +1,46 @@
 <?php // Example 05: signup.php
-  require_once 'header.php';
+require_once 'header.php';
 
 echo <<<_END
   <script>
-    function checkUser(user)
-    {
-      if (user.value == '')
-      {
-        $('#used').html('&nbsp;')
-        return
+    function checkUser(user) {
+      if (user.value == '') {
+        $('#used').html('&nbsp;');
+        return;
       }
 
-      $.post
-      (
+      $.post(
         'checkuser.php',
-        { user : user.value },
-        function(data)
-        {
-          $('#used').html(data)
+        { user: user.value },
+        function (data) {
+          $('#used').html(data);
         }
-      )
+      );
     }
   </script>  
 _END;
 
-  $error = $user = $pass = "";
-  if (isset($_SESSION['user'])) destroySession();
+$error = $user = $pass = "";
+if (isset($_SESSION['user'])) destroySession();
 
-  if (isset($_POST['user']))
-  {
+if (isset($_POST['user'])) {
     $user = sanitizeString($_POST['user']);
     $pass = sanitizeString($_POST['pass']);
 
-    if ($user == "" || $pass == "")
-      $error = 'Not all fields were entered<br><br>';
-    else
-    {
-      $result = queryMysql("SELECT * FROM members WHERE user='$user'");
+    if ($user == "" || $pass == "") {
+        $error = 'Not all fields were entered<br><br>';
+    } else {
+        $result = queryMysql("SELECT * FROM members WHERE user='$user'");
 
-      if ($result->rowCount())
-        $error = 'That username already exists<br><br>';
-      else
-      {
-        queryMysql("INSERT INTO members VALUES('$user', '$pass')");
-        die('<h4>Account created</h4>Please Log in.</div></body></html>');
-      }
+        if ($result->rowCount()) {
+            $error = 'That username already exists<br><br>';
+        } else {
+            // Insert into members table with explicit column names
+            queryMysql("INSERT INTO members (user, pass) VALUES ('$user', '$pass')");
+            die('<h4>Account created</h4>Please Log in.</div></body></html>');
+        }
     }
-  }
+}
 
 echo <<<_END
       <form method='post' action='signup.php?r=$randstr'>$error
@@ -56,8 +50,7 @@ echo <<<_END
       </div>
       <div data-role='fieldcontain'>
         <label>Username</label>
-        <input type='text' maxlength='16' name='user' value='$user'
-          onBlur='checkUser(this)'>
+        <input type='text' maxlength='16' name='user' value='$user' onBlur='checkUser(this)'>
         <label></label><div id='used'>&nbsp;</div>
       </div>
       <div data-role='fieldcontain'>
